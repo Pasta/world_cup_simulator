e22caa9c31916dfa36e871a42e343ad19ce54a91
Vincent Coste <coste.vincent@gmail.com>
2017-12-01 17:17:51 -0800

    displaying something

--- WorldCup.elm
+++ WorldCup.elm
@@ -1,6 +1,20 @@
 module WorldCup exposing (..)
 
-import Html exposing (text)
+import Dom
+import Html exposing (..)
+-- import Html.Attributes exposing (..)
+-- import Html.Events exposing (..)
+-- import Html.Keyed as Keyed
+-- import Html.Lazy exposing (lazy, lazy2)
+
+main : Program Never Model Msg
+main =
+  program
+      { init = init
+      , view = view
+      , update = update
+      , subscriptions = subscriptions
+      }
 
 type alias Model =
   { teams: List Team }
@@ -14,3 +28,22 @@ type Msg
 init : ( Model, Cmd Msg )
 init =
     ( { teams = ["Hello World Cup"] }, Cmd.none )
+    
+    
+view : Model -> Html Msg
+view model =
+  div
+    "Hello World Cup"
+  
+  
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+              
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+  
+  
